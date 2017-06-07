e500d649627e6afb295df4491de19d8bb0f00fc2
cjshaw <cjshaw@mit.edu>
2017-06-07 15:49:20 -0400

    add authoring tool selector for unlock previous; also add some tests for n of m

---
+++ client/js/middleware/superagent-qbank-mock-config.js
@@ -0,0 +1,46 @@
+// superagent-mock file for qbank calls
+// From: https://github.com/M6Web/superagent-mock
+module.exports = [
+  {
+    /**
+     * regular expression of URL
+     */
+    pattern: 'https://www.example.com(.*)',
+
+    /**
+     * returns the data
+     *
+     * @param match array Result of the resolution of the regular expression
+     * @param params object sent by 'send' function
+     * @param headers object set by 'set' function
+     * @param context object the context of running the fixtures function
+     */
+    fixtures: function (match, params, headers, context) {
+    },
+
+    /**
+     * returns the result of the GET request
+     *
+     * @param match array Result of the resolution of the regular expression
+     * @param data  mixed Data returns by `fixtures` attribute
+     */
+    get: function (match, data) {
+      return {
+        body: data
+      };
+    },
+
+    /**
+     * returns the result of the POST request
+     *
+     * @param match array Result of the resolution of the regular expression
+     * @param data  mixed Data returns by `fixtures` attribute
+     */
+    post: function (match, data) {
+      return {
+        code: 200,
+        body: data
+      };
+    }
+  },
+];
